bf1d55c0016da58ccfea3a0a3256b83b83b40a23
verster <rrverster31415@gmail.com>
2023-01-31 15:51:47 +0200

    icons and all that magic

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ import AiArt from './assets/img/ai_art.jpeg';
 
 import { PixelOptions, submitEvent } from './types';
 
+import { TfiLayoutGrid2, TfiLayoutGrid3, TfiLayoutGrid4 } from 'react-icons/tfi';
+import { RiLogoutBoxLine, RiDownloadFill } from 'react-icons/ri';
+
 function App() {
   const firebaseAuth = getAuth();
 
@@ -40,7 +43,7 @@ function App() {
 
   const generateImage = async (e: submitEvent) => {
     e.preventDefault();
-    setImg(Bee);
+    setImg('');
     setImgLoading(true);
     setError(false);
     try {
@@ -92,6 +95,16 @@ function App() {
     }
   };
 
+  const handleQualityChange = () => {
+    if (imgQuality === PixelOptions.small) {
+      setImgQuality(PixelOptions.medium);
+    } else if (imgQuality === PixelOptions.medium) {
+      setImgQuality(PixelOptions.large);
+    } else if (imgQuality === PixelOptions.large) {
+      setImgQuality(PixelOptions.small);
+    }
+  };
+
   const login = async (e: submitEvent) => {
     e.preventDefault();
     setAuthLoading(true);
@@ -109,6 +122,44 @@ function App() {
     }
     setAuthLoading(false);
   };
+
+  const renderStars = () => {
+    const starsList = [];
+    for (let i = 0; i < 3; i++) {
+      starsList.push(
+        <span className='magic-star'>
+          <svg viewBox='0 0 512 512'>
+            <path d='M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z' />
+          </svg>
+        </span>
+      );
+    }
+    return starsList;
+  };
+
+  let index = 0,
+    interval = 2500;
+
+  const rand = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const animate = (star) => {
+    star.style.setProperty('--star-left', `${rand(-100, 150)}%`);
+    star.style.setProperty('--star-top', `${rand(-70, 90)}%`);
+
+    star.style.animation = 'none';
+    star.offsetHeight;
+    star.style.animation = '';
+  };
+
+  const stars = document.querySelectorAll('.magic-star');
+
+  for (const star of stars) {
+    setTimeout(() => {
+      animate(star);
+
+      setInterval(() => animate(star), 1000);
+    }, index++ * (interval / 3));
+  }
 
   const logOut = async (e: submitEvent) => {
     e.preventDefault();
@@ -138,13 +189,15 @@ function App() {
         pauseOnHover
         theme='dark'
       />
-      <div className='container_genrated'>
-        <img
-          className='container_genrated__img'
-          onClick={() => copyImageToClipboard()}
-          src={img}
-          alt='Generated'
-        />
+      <div className='container_generated'>
+        {!imgLoading && (
+          <img
+            className='container_generated__img'
+            onClick={() => copyImageToClipboard()}
+            src={img}
+            alt={prompt}
+          />
+        )}
 
         {imgLoading && (
           <div className='loading-overlay'>
@@ -161,43 +214,36 @@ function App() {
               <div className='user_controls'>
                 <div className='form-container'>
                   <input value={prompt} onChange={(e) => setPrompt(e.target.value)}></input>
+                  {error && <p className='text_error'>Stop abusing this app!</p>}
+
                   <div className='container_controls__quality'>
                     <button
-                      className={`button_custom ${imgQuality === PixelOptions.small && 'active'}`}
-                      disabled={imgLoading}
-                      onClick={() => setImgQuality(PixelOptions.small)}
+                      className='button_custom button_controls quality'
+                      onClick={() => handleQualityChange()}
                     >
-                      Small
+                      {imgQuality === PixelOptions.small && <TfiLayoutGrid2 size={33} />}
+                      {imgQuality === PixelOptions.medium && <TfiLayoutGrid3 size={33} />}
+                      {imgQuality === PixelOptions.large && <TfiLayoutGrid4 size={33} />}
                     </button>
                     <button
-                      className={`button_custom button_medium ${
-                        imgQuality === PixelOptions.medium && 'active'
-                      }`}
+                      className='button_custom button_controls'
                       disabled={imgLoading}
-                      onClick={() => setImgQuality(PixelOptions.medium)}
+                      onClick={(e) => generateImage(e)}
                     >
-                      Medium
-                    </button>
-                    <button
-                      className={`button_custom ${imgQuality === PixelOptions.large && 'active'}`}
-                      disabled={imgLoading}
-                      onClick={() => setImgQuality(PixelOptions.large)}
-                    >
-                      Large
+                      <span className='magic'>
+                        {renderStars()}
+                        <span className='magic-text'>Generate</span>
+                      </span>
                     </button>
                   </div>
-                  <button className='button_custom' disabled={imgLoading} onClick={(e) => generateImage(e)}>
-                    Generate
-                  </button>
                 </div>
                 <button
                   className='button_custom button_logout'
                   disabled={authLoading}
                   onClick={(e) => logOut(e)}
                 >
-                  Log Out
+                  <RiLogoutBoxLine size={33} />
                 </button>
-                {error && <p className='text_error'>Stop abusing this app</p>}
               </div>
             ) : (
               <form className='form-container'>
