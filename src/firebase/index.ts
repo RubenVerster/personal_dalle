8c6957b4c90e376b5ca9213bf1ff1f96cf7ebb6e
Ruben Verster <37653127+RubenVerster@users.noreply.github.com>
2024-03-25 16:31:43 +0200

    BEE-001: update env config

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_Firebase_apiKey,
-  authDomain: import.meta.env.VITE_Firebase_authDomain,
-  projectId: import.meta.env.VITE_Firebase_projectId,
-  storageBucket: import.meta.env.VITE_Firebase_storageBucket,
-  messagingSenderId: import.meta.env.VITE_Firebase_messagingSenderId,
-  appId: import.meta.env.VITE_Firebase_appId,
+  apiKey: import.meta.env.Firebase_apiKey,
+  authDomain: import.meta.env.Firebase_authDomain,
+  projectId: import.meta.env.Firebase_projectId,
+  storageBucket: import.meta.env.Firebase_storageBucket,
+  messagingSenderId: import.meta.env.Firebase_messagingSenderId,
+  appId: import.meta.env.Firebase_appId,
 };
 
 const app = initializeApp(firebaseConfig);
